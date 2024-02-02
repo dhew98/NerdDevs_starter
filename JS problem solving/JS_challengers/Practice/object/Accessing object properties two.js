0f149acb151ec603ebfae96776e5ce62bc17bd90
dhew98 <jawaddhew98@gmail.com>
2024-02-02 16:45:21 +0600

    Check if property exists in object

--- JS problem solving/JS_challengers/Practice/object/Accessing object properties two.js
+++ JS problem solving/JS_challengers/Practice/object/Accessing object properties two.js
@@ -1,3 +1,11 @@
 function myFunction(obj) {
   return obj["prop-2"];
 }
+
+//another way
+
+// function myFunction(obj, key) {
+//   return obj[key];
+// }
+
+//myFunction({  continent: 'Asia',  country: 'Japan'}, 'continent')
